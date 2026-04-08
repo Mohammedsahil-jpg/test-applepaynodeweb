8acecb88363b708baf02145f9e0d8c31c27bb73d
mohamm-paypal <mohamm@paypal.com>
2026-04-08 14:29:11 +0530

    Test file

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static('public', { extensions: ['html'] }));
 
 // Serve the main page
 app.get('/', (req, res) => {
